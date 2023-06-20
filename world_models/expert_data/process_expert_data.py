ada7a2b2d626b8eb6bb7ef7a97b21b7a2a4450e2
reece <reece.omahoney3@gmail.com>
2023-06-20 15:41:24 +0100

    now read expert data from csv and also create init data

--- world_models/expert_data/process_expert_data.py
+++ world_models/expert_data/process_expert_data.py
@@ -24,7 +24,25 @@ def read_bag(bag_dir, bag_name, topics):
     return csvs
 
 
-data_dir = 'fwd_step'
-states = read_bag(data_dir, 'expert', ['state_and_action'])[0].to_numpy()[::16, 3:].astype(np.float32)
+data_dir = 'fwd_step_05_50'
+# states = read_bag(data_dir, 'expert', ['state_and_action'])[0].to_numpy()[::16, 3:].astype(np.float32)
+states = pd.read_csv(f'{data_dir}/expert.csv').to_numpy()[::8].astype(np.float32)
 states = states[:-(states.shape[0] % 64)]  # must be divisible by 64
-np.save(data_dir + '/expert_w_height', states)
+
+# init data
+init_data = np.zeros((states.shape[0], 49))
+init_data[:, 2] = states[:, 0]
+init_data[:, 3] = 1
+init_data[:, 7:19] = states[:, 4:16]
+
+# velocity
+init_data[:, 19:37] = states[:, 16:34]
+
+# action
+init_data[:, 37:] = states[:, 37:]
+
+np.savetxt(data_dir + '/init_data.csv', init_data, delimiter=",")
+
+# without height
+states = states[:, 1:]
+np.save(data_dir + '/expert', states)
