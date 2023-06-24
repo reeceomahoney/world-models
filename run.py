Add deterministic actions. Add world model validation script. Add test time ditto rewards to logger.py. Fixed velocity scaling in env

run.py
@@ -1,5 +1,4 @@
 import argparse
-import time
 from collections import OrderedDict
 from pathlib import Path
 
@@ -31,8 +30,8 @@ else:
 agent_state_dict = torch.load(agent_path, map_location=config.device)
 
 # backwards compatibility (remember to comment out ensemble)
-old_name = lambda key: key.startswith('actor') or key.startswith('critic') or key.startswith('slow')
-agent_state_dict = OrderedDict([('task_' + k, v) if old_name(k) else (k, v) for k, v in agent_state_dict.items()])
+# old_name = lambda key: key.startswith('actor') or key.startswith('critic') or key.startswith('slow')
+# agent_state_dict = OrderedDict([('task_' + k, v) if old_name(k) else (k, v) for k, v in agent_state_dict.items()])
 
 agent = Agent(*env_driver.env_info(), config)
 agent.load_state_dict(agent_state_dict, strict=False)
@@ -44,7 +43,7 @@ for _ in range(5):
     for step in range(500):
         timer.start()
         obs, reward, done = env_driver(action)
-        h_t, action = agent(h_t, obs)
+        h_t, action = agent(h_t, obs, deterministic=True)
         timer.end()
         if done or step == config.eval_steps - 1:
             obs, h_t, action = env_driver.reset()

train.py
@@ -37,9 +37,8 @@ agent = Agent(*env_driver.env_info(), config)
 if args.agent is not None:
     agent_state_dict = torch.load(home_path / args.agent, map_location=config.device)
     # These are for loading wms different to the current model, uncomment if needed
-    # agent_state_dict = OrderedDict([(k, v) for k, v in agent_state_dict.items() if not k.startswith('actor')])
     # agent_state_dict = OrderedDict([(k, v) for k, v in agent_state_dict.items() if 'actor' not in k])
-    agent_state_dict = OrderedDict([(k, v) for k, v in agent_state_dict.items() if 'critic' not in k])
+    # agent_state_dict = OrderedDict([(k, v) for k, v in agent_state_dict.items() if 'critic' not in k])
     agent.load_state_dict(agent_state_dict, strict=False)
 
 # replay buffer

world_models/agent.py
@@ -60,17 +60,19 @@ class Agent(torch.nn.Module):
     # --------------------------------------------------------------------------------------------------------------
     # Environment interaction
 
-    def __call__(self, h_t, obs):
+    def __call__(self, h_t, obs, deterministic=False):
         with torch.no_grad():
             obs = common.symlog(torch.Tensor(obs).to(self.device))
             z_t = self.world_model.encode(h_t, obs)
             action = self.actor(torch.cat((h_t, z_t), dim=-1)).sample()
+            if deterministic:
+                action = self.actor(torch.cat((h_t, z_t), dim=-1)).mode
             h_t1 = self.world_model.forward(h_t, z_t, action)
         return h_t1, action
 
     def predict(self, h_t, obs):
         with torch.no_grad():
-            h_t1, action = self(h_t, obs)
+            h_t1, action = self(h_t, obs, deterministic=True)
             z_t1 = self.world_model.dynamics(h_t1)
             obs_1, reward_1, cont_1 = self.world_model.predict(torch.cat((h_t1, z_t1), dim=-1))
         return (obs_1, reward_1, cont_1), h_t1, action
@@ -97,7 +99,7 @@ class Agent(torch.nn.Module):
     def ditto_step(self, replay):
         expert_sample = replay.sample(self.config.ditto_batch_size, self.config.imag_horizon + 1)
         expert_sample['state'][..., :self.config.h_dim] += \
-            torch.randn_like(expert_sample['state'][..., :self.config.h_dim]) * self.config.latent_noise_factor * 0.572
+            torch.randn_like(expert_sample['state'][..., :self.config.h_dim]) * self.config.latent_noise_factor * 0.44
         self.expert_actions = expert_sample['action']
 
         if self.config.ditto_state == 'logits':
@@ -126,7 +128,7 @@ class Agent(torch.nn.Module):
     # --------------------------------------------------------------------------------------------------------------
     # World Model
 
-    def train_world_model(self, replay):
+    def train_world_model(self, replay, train=True):
         # (seq_length, batch_size, obs_dim)
         data = next(replay)
         h_init = self._init_deter(data['obs'].shape[1]) if replay.idx == 1 else self.h_last
@@ -145,10 +147,11 @@ class Agent(torch.nn.Module):
         loss = pred_loss + dyn_loss + repr_loss
 
         # update weights
-        self.world_model_optim.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.world_model.parameters(), self.config.model_grad_clip)
-        self.world_model_optim.step()
+        if train:
+            self.world_model_optim.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.world_model.parameters(), self.config.model_grad_clip)
+            self.world_model_optim.step()
 
         info = {'pred_loss': pred_loss.item(), 'kl_loss': (dyn_loss + repr_loss).item()}
         return data, states, info

world_models/common/logger.py
@@ -146,21 +146,17 @@ class DittoLogger(Logger):
 
     def _eval(self, step):
         print('evaluating...')
-        self.eval_info = {'obs_error': 0, 'eval_reward': 0}
+        self.eval_info = {'obs_error': 0, 'eval_reward': torch.empty((0, 1)).to(self.config.device)}
         eval_driver = self.env_driver
         eval_driver.turn_on_visualization()
         timer = Timer(self.config.control_dt, self.config.real_time_eval)
+        n = self.config.eval_eps * self.config.eval_steps
 
+        agent_states = []
         for _ in range(self.config.eval_eps):
+            agent_states = self._calculate_eval_reward(eval_driver, agent_states)
             obs, h_t, _ = eval_driver.reset()
 
-            # get encoded expert data for reward
-            init_row = eval_driver.get_init_row() + 1
-            expert_data = {k: v[init_row:init_row + self.config.eval_steps] for k, v in self.expert_eval_data.items()}
-            expert_states = self.agent.encode_expert_data(expert_data)
-
-            n = self.config.eval_eps * self.config.eval_steps
-            agent_states = []
             for t in range(self.config.eval_steps):
                 timer.start()
                 preds, h_t, action = self.agent.predict(h_t, obs)
@@ -169,14 +165,29 @@ class DittoLogger(Logger):
                 agent_states.append(h_t)
                 timer.end()
                 if done.any():
+                    agent_states = self._calculate_eval_reward(eval_driver, agent_states)
                     obs, h_t, _ = eval_driver.reset()
-            agent_states = torch.stack(agent_states, dim=0)
 
-            # compute reward
-            expert_states = expert_states['state'][..., :self.config.h_dim]
-            reward = torch.sum(expert_states * agent_states, dim=-1)
-            reward /= (torch.maximum(torch.norm(expert_states, dim=-1), torch.norm(agent_states, dim=-1)) ** 2)
-            self.eval_info['eval_reward'] += reward.mean() / self.config.eval_eps
-
+        self.eval_info['eval_reward'] = self.eval_info['eval_reward'].mean()
         torch.save(self.agent.state_dict(), self.writer.log_dir + f'/../models/agent_{step}.pt')
         eval_driver.turn_off_visualization()
+
+    def _calculate_eval_reward(self, eval_driver, agent_states):
+        # skip this if agent states is empty
+        if len(agent_states) == 0:
+            return []
+
+        # get corresponding expert states
+        init_row = eval_driver.get_init_row() + 1
+        expert_data = {k: v[init_row:init_row + self.config.eval_steps] for k, v in self.expert_eval_data.items()}
+        expert_states = self.agent.encode_expert_data(expert_data)['state'][..., :self.config.h_dim]
+
+        # compute reward
+        agent_states = torch.stack(agent_states, dim=0)
+        expert_states = expert_states[:agent_states.shape[0]]
+        reward = torch.sum(expert_states * agent_states, dim=-1)
+        reward /= (torch.maximum(torch.norm(expert_states, dim=-1), torch.norm(agent_states, dim=-1)) ** 2)
+
+        # rewards are appended like this to we can weight them by the number of steps
+        self.eval_info['eval_reward'] = torch.cat((self.eval_info['eval_reward'], reward))
+        return []  # reset agent states