24c5c03f45180c254f74cabc471247e63c97cdf9
Reece O'Mahoney <reece.omahoney3@gmail.com>
2023-07-08 12:47:28 +0100

    PEP8 fixes

--- world_models/common/driver.py
+++ world_models/common/driver.py
@@ -1,12 +1,22 @@
 import os
 
+import torch
 import gymnasium as gym
 import world_models.raisim_gym as raisim_gym
 from ruamel.yaml import dump, RoundTripDumper
 
-from .utils import *
+from .utils import symlog
 
-to_np = lambda x: x.detach().cpu().numpy()
+
+def to_np(x):
+    return x.detach().cpu().numpy()
+
+
+def get_driver(config, config_dict=None):
+    if config.env_name == 'raisim':
+        return RaisimDriver(config, config_dict)
+    else:
+        return GymDriver(config)
 
 
 class DriverBase:
@@ -17,9 +27,12 @@ class DriverBase:
 
     def _init_deter(self):
         if self.config.init_deter == 'zero':
-            return torch.zeros((self.config.num_envs, self.config.h_dim)).to(self.config.device)
+            return torch.zeros((self.config.num_envs, self.config.h_dim)).to(
+                self.config.device)
         elif self.config.init_deter == 'normal':
-            return 0.01 * torch.randn((self.config.num_envs, self.config.h_dim)).to(self.config.device)
+            return 0.01 * torch.randn((
+                self.config.num_envs, self.config.h_dim)).to(
+                    self.config.device)
 
     def _to_ten(self, x):
         return torch.tensor(x, dtype=torch.float32).to(self.config.device)
@@ -59,38 +72,50 @@ class GymDriver(DriverBase):
         self._env.close()
 
     def _make_env(self):
-        kwargs = {}
-        wrappers = None
         if self._config.record:
-            kwargs = {'render_mode': 'rgb_array'}
-            video_path = os.path.dirname(os.path.realpath(__file__)) + f'/../../logs/{self._config.env_name}/videos'
-            wrappers = lambda x: gym.wrappers.RecordVideo(x, video_path, episode_trigger=lambda y: True)
+            video_path = os.path.dirname(os.path.realpath(__file__)) + \
+                f'/../../logs/{self._config.env_name}/videos'
+            self._env = gym.vector.make(
+                self._config.env_name,
+                render_mode='rgb_array',
+                num_envs=self._config.num_envs,
+                wrappers=lambda x: self._wrapper(x, video_path))
         elif self._render:
-            kwargs = {'render_mode': 'human'}
-        self._env = gym.vector.make(self._config.env_name, **kwargs, num_envs=self._config.num_envs, wrappers=wrappers)
+            self._env = gym.vector.make(
+                self._config.env_name,
+                render_mode='human',
+                num_envs=self._config.num_envs)
+
+    def _wrapper(self, x, video_path):
+        return gym.wrappers.RecordVideo(x, video_path,
+                                        episode_trigger=lambda y: True)
 
 
 class RaisimDriver(DriverBase):
     def __init__(self, config, config_dict):
         super(RaisimDriver, self).__init__(config)
         self._raisim_config = config_dict
-        rsc_path = os.path.dirname(os.path.realpath(__file__)) + '/../raisim_gym/rsc'
+        rsc_path = os.path.dirname(os.path.realpath(__file__)) + \
+            '/../raisim_gym/rsc'
         self._env = raisim_gym.VecEnv(raisim_gym.RaisimGymEnv(
-            rsc_path, dump(self._raisim_config, Dumper=RoundTripDumper)), normalize_ob=False)
+            rsc_path, dump(self._raisim_config, Dumper=RoundTripDumper)),
+                                      normalize_ob=False)
         self._env.turn_off_visualization()
 
     def __call__(self, action):
         self.step += 1
         obs = symlog(self._to_ten(self._env.observe()))
         reward, done = self._env.step(to_np(action))
-        return obs, self._to_ten(reward).unsqueeze(-1), self._to_ten(done).unsqueeze(-1)
+        return obs, self._to_ten(reward).unsqueeze(-1), \
+            self._to_ten(done).unsqueeze(-1)
 
     def reset(self):
         self.step = 0
         self._env.reset()
         h_t = self._init_deter()
         obs = self._to_ten(self._env.observe())
-        action = torch.randn(self.config.num_envs, self._env.num_acts).to(self.config.device)
+        action = torch.randn(self.config.num_envs, self._env.num_acts).to(
+            self.config.device)
         return obs, h_t, action
 
     def env_info(self):
@@ -110,10 +135,3 @@ class RaisimDriver(DriverBase):
 
     def get_init_row(self):
         return self._env.get_init_row()
-
-
-def get_driver(config, config_dict=None):
-    if config.env_name == 'raisim':
-        return RaisimDriver(config, config_dict)
-    else:
-        return GymDriver(config)
