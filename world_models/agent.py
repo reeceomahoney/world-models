Fix eval reward calculation
Previously the eval ditto reward was only being calculated on the
deterministic part of the hidden state, regardless of the training time
ditto state. This has now been fixed and will match the training state
if its 'deter' or 'full'. NB: ditto state 'logits' still not implemented
    at eval

world_models/agent.py
@@ -94,9 +94,9 @@ class Agent(torch.nn.Module):
         with torch.no_grad():
             h_t1, action = self(h_t, obs, deterministic=True)
             z_t1 = self.world_model.dynamics(h_t1)
-            obs_1, reward_1, cont_1 = self.world_model.predict(
-                torch.cat((h_t1, z_t1), dim=-1))
-        return (obs_1, reward_1, cont_1), h_t1, action
+            state = torch.cat((h_t1, z_t1), dim=-1)
+            obs_1, reward_1, cont_1 = self.world_model.predict(state)
+        return (obs_1, reward_1, cont_1), state, action
 
     # -------------------------------------------------------------------------
     # Training

world_models/common/visualizer.py
@@ -60,11 +60,12 @@ class Visualizer:
 
             for t in range(self.config.eval_steps):
                 timer.start()
-                preds, h_t, action = self.agent.predict(h_t, obs)
+                preds, state, action = self.agent.predict(h_t, obs)
                 obs, reward, done = self.env_driver(action)
                 self.eval_info['obs_error'] += torch.norm(preds[0] - obs) / n
                 mean_reward += reward / n
-                agent_states.append(h_t)
+                agent_states.append(state)
+                h_t = state[..., :self.config.h_dim]
                 timer.end()
                 if done.any():
                     agent_states = self._calculate_ditto_reward(agent_states)
@@ -92,10 +93,12 @@ class Visualizer:
         expert_data = {
             k: v[start_idx:start_idx + self.config.eval_steps, eps_idx]
             for k, v in self.expert_eval_data.items()}
-        expert_states = self.agent.encode_expert_data(
-            expert_data)['state'][..., :self.config.h_dim]
-
+        expert_states = self.agent.encode_expert_data(expert_data)['state']
         agent_states = torch.stack(agent_states, dim=0)
+
+        if self.config.ditto_state == 'deter':
+            expert_states = expert_states[..., :self.config.h_dim]
+            agent_states = agent_states[..., :self.config.h_dim]
 
         # to deal with early termination
         expert_states = expert_states[:agent_states.shape[0]]